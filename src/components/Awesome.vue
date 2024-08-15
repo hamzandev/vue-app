7523cbedc203cb5fd893be1a58a5692841a4bd26
hamzandev <hamzankechil@gmail.com>
2024-08-15 12:38:35 +0800

    Topic: conditional rendering

---
+++ src/components/Awesome.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Button from './Button.vue';
+
+
+const isAwesome = ref<boolean>(false)
+function handleChangeAwesome() {
+    isAwesome.value = !isAwesome.value
+}
+</script>
+
+<template>
+    <div class="flex flex-col items-center gap-2 mx-auto my-5 max-w-xs">
+        <span class="font-bold text-2xl text-green-500" v-if="isAwesome">You're an awesome Guy! ✨</span>
+        <span class="font-bold text-2xl text-green-500" v-else>You're a normal Guy 😐</span>
+
+        <Button @click="handleChangeAwesome" class="w-max">
+            {{ isAwesome ? 'Make me normal guy' : 'I wanna be awesome' }}
+        </Button>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-  import Counter from '@/components/Counter.vue';
+import Awesome from '@/components/Awesome.vue';
+import Counter from '@/components/Counter.vue';
 import Person from '@/components/Person.vue';
 </script>
 
 <template>
   <Counter />
-  <Person/>
+  <Person />
+  <Awesome />
 </template>
