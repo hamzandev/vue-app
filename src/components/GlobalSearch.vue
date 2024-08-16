3de87f74ae4406794bd1fbe395db9c1e717d1d58
hamzandev <hamzankechil@gmail.com>
2024-08-16 19:02:07 +0800

    Topic: Templates ref

---
+++ src/components/GlobalSearch.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const input = ref(null as any)
+// implementation of template refs to <input/> tag
+onMounted(() => {
+    input.value.focus()
+})
+</script>
+
+<template>
+    <form class="flex items-center">
+        <input type="search" ref="input" placeholder="🔍 Quick Search..."
+            class="border-gray-400 mb-3 p-2 border rounded-lg focus-within:ring focus-within:ring-green-300 focus-within:outline-none">
+    </form>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,21 @@
+<script setup lang="ts">
+import GlobalSearch from "@/components/GlobalSearch.vue"
+</script>
+
 <template>
-    <header class="top-0 sticky bg-white/80 backdrop-blur py-3 border-b border-b-gray-200">
-        <div class="flex items-center space-x-8 mx-auto container">
-            <div id="logo" class="flex items-center space-x-1 me-10">
+    <header class="top-0 sticky bg-white/80 backdrop-blur py-2 border-b border-b-gray-200">
+        <div class="flex justify-between items-center space-x-8 mx-auto container">
+            <div id="logo" class="flex items-center space-x-1 w-2/12">
                 <span class="text-xl">⚡</span>
                 <span class="font-bold text-green-500 uppercase">Vue App</span>
             </div>
-            <div id="menus" class="flex items-center space-x-5 text-gray-500">
+            <div id="menus" class="flex items-center space-x-5 w-8/12 text-gray-500">
                 <RouterLink to="/">Home</RouterLink>
                 <RouterLink to="/courses">Courses</RouterLink>
                 <RouterLink to="/about">About</RouterLink>
+            </div>
+            <div id="global-search" class="flex items-center w-2/12">
+                <GlobalSearch />  
             </div>
         </div>
     </header>
